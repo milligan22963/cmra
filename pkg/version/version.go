09d5b5a499e33de64b28a3b642f039403c14cc6c
Dan Milligan <dwm@afmsoftware.com>
2021-07-25 19:20:56 -0400

    Updated task options and version output

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -6,10 +6,12 @@ import "github.com/sirupsen/logrus"
 // VersionString is what is assigned during build if at all
 // to allow the user to display what they are currently running
 var VersionString = "development"
+var BuildTime = "dev"
 
 type VersionInstance struct {
 }
 
 func (version *VersionInstance) Run() {
-	logrus.Infof("version: \n\t%s", VersionString)
+	logrus.Infof("Software Version: \t%s\n", VersionString)
+	logrus.Infof("Build Time: \t%s", BuildTime)
 }
