c3baae6832826a1b5bbb7d9b9084cccd3d6b79a5
Dan Milligan <dwm@afmsoftware.com>
2021-07-24 21:26:56 -0400

    Getting lint in place and task file

--- cmd/main.go
+++ cmd/main.go
@@ -2,26 +2,25 @@ package main
 
 import (
 	"github.com/milligan22963/cmra/cmd/subcmd"
-
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "AFMCamera",
-	Short: "A camera application",
-	Long:  `An application to interface with the pi camera and give a web view for it`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
-	},
-}
-
-func init() {
-
-}
-
 func main() {
 	logrus.Info("starting up")
+
+	rootCmd := &cobra.Command{
+		Use:   "AFMCamera",
+		Short: "A camera application",
+		Long:  `An application to interface with the pi camera and give a web view for it`,
+		Run: func(cmd *cobra.Command, args []string) {
+			// Do Stuff Here
+		},
+	}
+
 	rootCmd.AddCommand(subcmd.ServerCmd)
-	rootCmd.Execute()
+
+	if err := rootCmd.Execute(); err != nil {
+		logrus.Errorf("error executing cmd: %v", err)
+	}
 }
